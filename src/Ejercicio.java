5f92cc12b3f34ef984abc7e3a1f9a18885733562
KaOSrp19 <kevinramos28101999@hotmail.com>
2021-11-17 08:31:58 -0400

    Busuqueda Binaria y Secuencial

--- src/Ejercicio.java
+++ src/Ejercicio.java
@@ -72,5 +72,64 @@ public class Ejercicio {
     }
 
 
+    //Indice vector
+    public static int indiceVector(int []vector,int indice){
+        int contador = 0;
+        if (indice == vector.length - 1){ //caso base
+            if (indice == vector[indice]){
+                contador ++;
+            }
+        }else {  //llamada recursiva
+            if (indice == vector[indice]){
+                contador++;
+            }
+            contador = contador +indiceVector(vector , indice + 1);
+        }
+        return contador;
+
+    }
+    //metodo auxiliar
+    public static boolean indiceVector(int[] vector){
+        return  indiceVector(vector,0)>0;
+    }
+
+
+    //Indice vector 2
+
+
+    //busqueda binaria
+    //busqueda secuencial
+
+        /** Buscar tablas desordenadas Búsqueda secuencial */
+        public static int busquedaSecuencial(int[] listaDatos, int objeto) {
+            for (int i = 0; i < listaDatos.length; i++) {
+                if (listaDatos[i] == objeto)
+                    return i;
+            }
+            return -1;
+        }
+
+        /**
+         * Búsqueda binaria, para listas ordenadas
+         */
+        public static int busquedaBinaria(int[] listaDatos, int objeto) {
+            int mid = 0;
+            int first = 0;
+            int last = listaDatos.length - 1;
+
+            while (first <= last) {
+                mid = (first + last) / 2;
+                if (objeto == listaDatos[mid])
+                    return mid;
+                else {
+                    if (objeto > listaDatos[mid])
+                        first = mid + 1;
+                    else
+                        last = mid - 1;
+                }
+            }
+            return -1;
+        }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -21,8 +21,23 @@ public class Main {
         System.out.println(" ");*/
 
         //maximo de un vector
-        int[] vector = {2, 9, 6, 7, 6, 3, 4, 3};
-        System.out.println(Ejercicio.maximoVector(vector));
+        /*int[] vector = {2, 9, 6, 7, 6, 3, 4, 3};
+        System.out.println(Ejercicio.maximoVector(vector));*/
+
+        //indicevector
+        /*int[] vector={10,8,2,6,4,2,1,0};
+        System.out.println(Ejercicio.indiceVector(vector));*/
+
+        //indicevector2
+
+
+        int datalist1[] = {23, 56, 73, 77, 26, 65, 41, 34, 12, 74, 37};
+        int datalist2[] = {23, 28, 33, 34, 41, 47, 59, 71, 77, 82, 90};
+
+        System.out.println(Ejercicio.busquedaSecuencial(datalist1,77));
+        //System.out.println(Ejercicio.busquedaBinaria(datalist2,33));
+
+
 
 
     }
